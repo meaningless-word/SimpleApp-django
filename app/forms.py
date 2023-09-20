7b76959bf2359dc45db68d1315611e47b5cfc8cc
meaningless-word <grisly@mail.ru>
2023-09-20 23:19:01 +0700

    валидация

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    description = forms.CharField(min_length=20)
+
     class Meta:
         model = Product
         ## fields = '__all__' # не очень хороший путь - в таблицах бывают поля, которые непозволительно отдавать на редактирование пользователю
@@ -13,3 +17,28 @@ class ProductForm(forms.ModelForm):
             'price',
             'quantity',
         ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        description = cleaned_data.get('description')
+        # if description is not None and len(description) < 20:
+        #     raise ValidationError({
+        #         "description": "Описание не может быть менее 20 символов."
+        #     })
+
+        name = cleaned_data.get('name')
+        if name == description:
+            raise ValidationError(
+                "Описание не должно быть идентичным названию."
+            )
+
+        return cleaned_data
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        if name[0].islower():
+            raise ValidationError(
+                "Название должно начинаться с заглавной буквы."
+            )
+        return name
