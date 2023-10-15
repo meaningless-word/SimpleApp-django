7d91c94819f7ad05f1d4ef96d5e83d679e806ea1
meaningless-word <grisly@mail.ru>
2023-10-16 00:41:19 +0700

    using celery

--- config/settings.py
+++ config/settings.py
@@ -189,3 +189,10 @@ MANAGERS = (
 ADMINS = (
     ('anton', 'ant.on@non-existen-mail.ru'),
 )
+
+# Настройки для Celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
