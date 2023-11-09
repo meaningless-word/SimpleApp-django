22d3f95c5c74ffcdd7739e7464b2923feaa2686c
meaningless-word <grisly@mail.ru>
2023-11-09 21:17:00 +0400

    adjust admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Category, Product
 
+
+# напишем уже знакомую нам функцию обнуления товара на складе
+def nullfy_quantity(modeladmin, request, queryset): # все аргументы уже должны быть вам знакомы, самые нужные из них это request — объект хранящий информацию о запросе и queryset — грубо говоря набор объектов, которых мы выделили галочками.
+    queryset.update(quantity=0)
+nullfy_quantity.short_description = 'Обнулить товары' # описание для более понятного представления в админ панели задаётся, как будто это объект
+
+
+# создаём новый класс для представления товаров в админке
+class ProductAdmin(admin.ModelAdmin):
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    # list_display = [field.name for field in Product._meta.get_fields()]  # генерируем список имён всех полей для более красивого отображения
+    list_display = ('name', 'description', 'price', 'quantity', 'category', 'on_stock',)
+    list_filter = ('price', 'quantity', 'name')  # добавляем примитивные фильтры в нашу админку
+    search_fields = ('name', 'category__name')  # тут всё очень похоже на фильтры из запросов в базу
+    actions = [nullfy_quantity]  # добавляем действия в список
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Product)
-# admin.site.register()
+admin.site.register(Product, ProductAdmin)
